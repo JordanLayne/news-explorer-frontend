0477d4f8b317174f1fa226aea16a7320848fbff1
Jordan Layne <jordanmlayne2001@gmail.com>
2023-06-01 12:51:10 -0400

    initial commit

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -7,15 +7,15 @@ const About = () => {
       <div>
         <h1 className="about-title">About the author</h1>
         <p className="about-text">
-          Hello, I'm Jordan, a full-stack developer proficient in HTML, CSS,
-          JavaScript, React, Firebase, Django, Tailwind CSS, TypeScript,
-          Node.js, and Python. With expertise in creating visually appealing
-          user interfaces, developing robust server-side solutions using Django,
-          managing databases, designing RESTful APIs, and integrating Firebase
-          for seamless functionality, I deliver exceptional results across the
-          full-stack development process. If you need a skilled developer in
-          React, Django, Firebase, Tailwind CSS, TypeScript, and more, feel free
-          to reach out. I'm excited to help you achieve your goals.
+          I'm Jordan Layne, a software developer specializing in web development.
+          With expertise in JavaScript, React, Node.js, and MongoDB, I enjoy
+          creating user-friendly and innovative applications. I successfully
+          completed the Practicum Software Developer program, where I gained
+          hands-on experience in building real-world projects. My skills include
+          developing interactive web applications, implementing RESTful APIs,
+          and managing databases. I prioritize clean code, adhere to industry
+          standards, and am committed to delivering high-quality solutions.
+          Let's collaborate and bring your ideas to life!
         </p>
       </div>
     </Wrapper>
