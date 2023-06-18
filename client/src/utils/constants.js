d846dd67ac45e05e768eb597cd6116f5d17e4579
Jordan Layne <jordanmlayne2001@gmail.com>
2023-06-18 03:40:06 -0400

    part 3

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,14 +1,11 @@
 export const processRes = (res) => {
-    if (res.ok) {
-      return res.json();
-    }
-    return Promise.reject(`Error: ${res.status}`);
-  };
-  
- 
-  
-  export const baseUrl = process.env.NODE_ENV === 'production' 
-  ? 'https://api.news-explorer.twilightparadox.com'
-  : 'http://localhost:3000';
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Error: ${res.status}`);
+};
 
-  
+export const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.news-explorer.twilightparadox.com"
+    : "http://localhost:3000";
